9d13eeeb94e26fa891189619c930f57b3c6c16f8
kiraGit67 <kirsten@sais.de>
2024-01-01 18:53:28 +0100

    adds PlanPickerComponent, removes global registration

--- 02-plan-picker/app.js
+++ 02-plan-picker/app.js
@@ -6,6 +6,45 @@ let PlanComponent = {
   },
 };
 
+let PlanPickerComponent = {
+  components: { plan: PlanComponent },
+  template: "#plan-picker-template",
+  data() {
+    return {
+      plans: [
+        {
+          name: "The Single",
+          price: 1.0,
+        },
+        {
+          name: "The Curious",
+          price: 1.15,
+        },
+        {
+          name: "The Addict",
+          price: 1.25,
+        },
+        {
+          name: "The Enjoyful",
+          price: 1.2,
+        },
+        {
+          name: "The Grateful",
+          price: 1.1,
+        },
+        {
+          name: "The Hacker",
+          price: 1.35,
+        },
+        {
+          name: "The Gambler",
+          price: 1.5,
+        },
+      ],
+    };
+  },
+};
+
 const app = Vue.createApp({
   data() {
     return {
@@ -41,43 +80,4 @@ const app = Vue.createApp({
       ],
     };
   },
-})
-  .component("plan-picker", {
-    components: { plan: PlanComponent },
-    template: "#plan-picker-template",
-    data() {
-      return {
-        plans: [
-          {
-            name: "The Single",
-            price: 1.0,
-          },
-          {
-            name: "The Curious",
-            price: 1.15,
-          },
-          {
-            name: "The Addict",
-            price: 1.25,
-          },
-          {
-            name: "The Enjoyful",
-            price: 1.2,
-          },
-          {
-            name: "The Grateful",
-            price: 1.1,
-          },
-          {
-            name: "The Hacker",
-            price: 1.35,
-          },
-          {
-            name: "The Gambler",
-            price: 1.5,
-          },
-        ],
-      };
-    },
-  })
-  .mount("#app");
+}).mount("#app");
