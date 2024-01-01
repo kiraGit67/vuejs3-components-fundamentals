fa063e086586e390f2f0207a361bcbf0bb01bb6c
kiraGit67 <kirsten@sais.de>
2024-01-01 18:36:27 +0100

    adds 02-plan-picker/app-copy.js as a safety copy

---
+++ 02-plan-picker/app-copy.js
@@ -0,0 +1,81 @@
+Vue.createApp({
+  data() {
+    return {
+      plans: [
+        {
+          name: "The Single",
+          price: 1.0,
+        },
+        {
+          name: "The Curious",
+          price: 1.15,
+        },
+        {
+          name: "The Addict",
+          price: 1.25,
+        },
+        {
+          name: "The Enjoyful",
+          price: 1.2,
+        },
+        {
+          name: "The Grateful",
+          price: 1.1,
+        },
+        {
+          name: "The Hacker",
+          price: 1.35,
+        },
+        {
+          name: "The Gambler",
+          price: 1.5,
+        },
+      ],
+    };
+  },
+})
+  .component("plan", {
+    template: "#plan-template",
+    props: {
+      name: { type: String, default: "The Single", required: true },
+      price: { type: Number, default: 1.0, required: false },
+    },
+  })
+  .component("plan-picker", {
+    template: "#plan-picker-template",
+    data() {
+      return {
+        plans: [
+          {
+            name: "The Single",
+            price: 1.0,
+          },
+          {
+            name: "The Curious",
+            price: 1.15,
+          },
+          {
+            name: "The Addict",
+            price: 1.25,
+          },
+          {
+            name: "The Enjoyful",
+            price: 1.2,
+          },
+          {
+            name: "The Grateful",
+            price: 1.1,
+          },
+          {
+            name: "The Hacker",
+            price: 1.35,
+          },
+          {
+            name: "The Gambler",
+            price: 1.5,
+          },
+        ],
+      };
+    },
+  })
+  .mount("#app");
