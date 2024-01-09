ab1749059e31e4ac3710ae526537de026006e823
kiraGit67 <kirsten@sais.de>
2024-01-09 19:09:59 +0100

    adds data() and .component() functions

--- 04-github-user-profile/app.js
+++ 04-github-user-profile/app.js
@@ -1,1 +1,28 @@
-Vue.createApp({}).mount("#app");
+Vue.createApp({
+  data() {
+    return {
+      usernames: ["danielkellyio", "hootlex", "rahaug", "akryum"],
+    };
+  },
+})
+  .component("github-user-card", {
+    template: "#github-user-card-template",
+    props: {
+      username: {
+        type: String,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        user: {},
+      };
+    },
+    async created() {
+      const response = await axios.get(
+        `https://api.github.com/users/${this.username}`
+      );
+      this.user = response.data;
+    },
+  })
+  .mount("#app");
