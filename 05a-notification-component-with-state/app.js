78f5356bb8ba910b64735c3db23a02122d247986
kiraGit67 <kirsten@sais.de>
2024-01-13 22:07:31 +0100

    adds time property to each message

--- 05a-notification-component-with-state/app.js
+++ 05a-notification-component-with-state/app.js
@@ -5,27 +5,31 @@ Vue.createApp({
       messages: [
         {
           id: 1,
+          time: "08:00 am",
           type: "info",
           header: "Good Morning!",
           text: "Enjoy your flight!",
         },
         {
           id: 2,
+          time: "09:45 am",
           type: "success",
           header: "Ah Yeah!",
           text: "Your flight will be in time!",
         },
         {
           id: 3,
+          time: "01:05 pm",
           type: "warning",
           header: "Attention!",
-          text: "Your flight will arrive with a delay of 30 to 60 minutes.",
+          text: "Your flight to Dubai will arrive with a delay of 30 to 60 minutes.",
         },
         {
           id: 4,
+          time: "01:15 pm",
           type: "error",
           header: "Oh No!",
-          text: "Your flight has just been cancelled. Look onto the departure table for an alternative flight.",
+          text: "Your flight to New York City has just been cancelled. Look onto the departure table for an alternative flight.",
         },
       ],
     };
