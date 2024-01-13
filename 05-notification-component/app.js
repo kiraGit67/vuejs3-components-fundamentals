5c3a145dddec171a71da20e2ea4b60a6041a2873
kiraGit67 <kirsten@sais.de>
2024-01-13 12:53:28 +0100

    adds component "notification-message", template: "#notification-message-template"

--- 05-notification-component/app.js
+++ 05-notification-component/app.js
@@ -1,1 +1,25 @@
-Vue.createApp({}).mount("#app");
+Vue.createApp({})
+  .component("notification-message", {
+    template: "#notification-message-template",
+    props: {
+      type: {
+        type: String,
+        default: "info",
+      },
+      header: {
+        type: String,
+        default: "Information",
+      },
+    },
+    data() {
+      return {
+        hidden: false,
+      };
+    },
+    methods: {
+      hide() {
+        this.hidden = true;
+      },
+    },
+  })
+  .mount("#app");
